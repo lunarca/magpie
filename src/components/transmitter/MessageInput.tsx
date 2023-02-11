e9ff92f5a51aa7c008738b2bc3b7f37bc2176601
Alex (LunarCA) <alexdefreese@gmail.com>
2023-02-11 17:10:42 -0500

    Add message input element

---
+++ src/components/transmitter/MessageInput.tsx
@@ -0,0 +1,21 @@
+import { Component } from 'solid-js';
+import Panel from '../Panel';
+
+type Props = {
+  setMessage: (message: string) => void
+}
+
+const MessageInput: Component<Props> = (props) => {
+  return (
+    <div class="w-96 h-64">
+      <Panel title="Message Input">
+        <textarea 
+          onChange={(event) => {props.setMessage(event.currentTarget.value)}} 
+          class="bg-stone-800 h-full w-full resize-none outline-none overflow-auto" />
+      </Panel>
+    </div>
+
+  )
+}
+
+export default MessageInput
--- src/pages/radios/Transmitter.tsx
+++ src/pages/radios/Transmitter.tsx
@@ -1,12 +1,20 @@
-import { Component } from 'solid-js';
+import { Component, createEffect, createSignal } from 'solid-js';
+import MessageInput from '../../components/transmitter/MessageInput';
 
 type Props = {
 
 }
 const Transmitter: Component<Props> = (props) => {
+
+  const [message, setMessage] = createSignal("")
+
+  createEffect(() => {
+    console.log(message())
+  })
+
   return (
-    <div>
-      <h1>Transmitter</h1>
+    <div class="grid">
+      <MessageInput setMessage={setMessage}/>
     </div>
   )
 }
