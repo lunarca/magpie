b20061f9543573243d900ed9fd8251f6809c9e96
Alex DeFreese <alexdefreese@gmail.com>
2023-02-11 12:31:24 -0500

    Add Firebase webapp config

---
+++ src/libs/firebase.ts
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyC9UKVbK5jp1XoV7XTAiRjHi6GUCrOxOxw",
+
+  authDomain: "magpie-ecb0a.firebaseapp.com",
+
+  projectId: "magpie-ecb0a",
+
+  storageBucket: "magpie-ecb0a.appspot.com",
+
+  messagingSenderId: "106194819654",
+
+  appId: "1:106194819654:web:c2b1006c7124b864348847"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
