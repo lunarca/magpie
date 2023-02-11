675039daf5ca25c69ca95b695cc22a3ba2f4c405
Alex (LunarCA) <alexdefreese@gmail.com>
2023-02-11 14:58:51 -0500

    Clean up deps

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import type { Component } from 'solid-js';
 
-import logo from './logo.svg';
-import styles from './App.module.css';
 import Panel from './components/Panel';
 
 const App: Component = () => {
