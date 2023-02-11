a1354b9618112ea3953fa5f4dd10057c5aa5fbd9
Alex (LunarCA) <alexdefreese@gmail.com>
2023-02-11 16:51:44 -0500

    Fix Panel sizing

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -10,9 +10,9 @@ const Panel: Component<Props> = (props: Props) => {
   const c = children(() => props.children)
 
   return (
-    <div class="m-2 border-orange-500 border-solid border-2 min-w-80 min-h-50 shadow">
+    <div class="m-2 border-orange-500 border-solid border-2 shadow w-full h-full  bg-stone-800 flex flex-col">
       <div class="bg-orange-500 text-slate-900 px-2 py-1 font-bold">{props.title}</div>
-      <div class="p-2 text-gray-200 min-h-40 bg-stone-800">{c()}</div>
+      <div class="p-2 text-gray-200 w-full h-full">{c()}</div>
     </div>
   )
 }
