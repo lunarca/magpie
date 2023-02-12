ff75c3ff5d882f43f21608b6864a0517997eff86
Alex (LunarCA) <alexdefreese@gmail.com>
2023-02-12 11:40:31 -0500

    Add logo and icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Component, lazy } from 'solid-js';
 import { Routes, Route } from "@solidjs/router";
+import Logo from './components/Logo';
 
 const Index = lazy(() => import('./pages/Index'));
 
 const App: Component = () => (
   <>
+    <Logo />
     <Routes>
       <Route path="/" component={Index} />
       <Route path="/tx" component={lazy(() => import('./pages/radios/TransmitterPage'))} />
---
+++ src/components/Logo.tsx
@@ -0,0 +1,19 @@
+import { Component } from 'solid-js';
+
+type Props = {
+
+}
+
+const Logo: Component<Props> = (props) => {
+  return (
+    <div class="flex flex-row h-16 w-fit items-center justify-center absolute mx-5 my-3 flex-2 gap-2">
+      <span class="flex flex-row font-extrabold h-fit text-4xl">
+        <div class="text-orange-500">MAG</div>
+        <div class="text-cyan-400">PIE</div>
+      </span>
+      <img class="h-fit" src="/src/assets/icons/magpie-icon.png"></img>
+    </div>
+  )
+}
+
+export default Logo
