5766ed237187b0959fae96be67f98cc2394aba75
Alex (LunarCA) <alexdefreese@gmail.com>
2023-02-12 13:52:05 -0500

    Correct capitalization on index page file

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 /* @refresh reload */
 import { render } from 'solid-js/web';
 
-import './index.css';
+import './Index.css';
 import App from './App';
 import { FirebaseProvider } from 'solid-firebase';
 import { firebaseConfig } from './libs/firebase';
