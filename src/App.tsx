36434d8af03a86a4c70632269f855ee3a48ef23b
Alex (LunarCA) <alexdefreese@gmail.com>
2023-02-18 15:55:38 -0500

    Remove container class on root

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const App: Component = () => (
   <div class="bg-stone-900 absolute h-screen w-screen scanlines">
     <Logo />
 
-    <div class="w-full h-content-wrapper container overflow-auto">
+    <div class="w-full h-content-wrapper overflow-auto">
       <Routes>
         <Route path="/" component={lazy(() => import('./pages/IndexPage'))} />
         <Route path="/tx" component={lazy(() => import('./pages/radios/TransmitterPage'))} />
