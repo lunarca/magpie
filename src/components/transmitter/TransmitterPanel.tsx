76dbb2ae30a21842592c5b47ec8b1faa22e4ea33
Alex (LunarCA) <alexdefreese@gmail.com>
2023-02-18 17:12:59 -0500

    Initial work on tuner panel

--- src/components/transmitter/TransmitterPanel.tsx
+++ src/components/transmitter/TransmitterPanel.tsx
@@ -1,6 +1,7 @@
 import { Component } from 'solid-js';
 import Panel from '../Panel';
 import Button from '../util/Button';
+import Tuner from '../util/Tuner';
 
 type Props = {
   testSignal: () => void,
@@ -12,14 +13,21 @@ const TransmitterPanel: Component<Props> = (props) => {
   return (
     <div class="h-full">
       <Panel title="Transmitter">
-        <div class="flex flex-row justify-between">
-          <Button color="primary" onClick={props.testSignal}>Test Signal</Button>
-          <Button color="secondary" onClick={props.stopTest}>Stop Audio</Button>
+
+        <div class="flex flex-col gap-2">
+
+          <Tuner frequency={127.300} />
+
+          <div class="flex flex-row justify-between">
+            <Button color="primary" onClick={props.testSignal}>Test Signal</Button>
+            <Button color="secondary" onClick={props.stopTest}>Stop Audio</Button>
+          </div>
+          
+          <div class="w-full flex">
+            <Button color="RED" flexGrow onClick={props.transmitSignal}>Transmit Signal</Button>
+          </div>
         </div>
         
-        <div class="w-full flex">
-          <Button color="RED" flexGrow onClick={props.transmitSignal}>Transmit Signal</Button>
-        </div>
 
       </Panel>
     </div>
