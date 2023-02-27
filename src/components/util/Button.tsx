41a8900416ec927e84fb1385117d75378a267687
Alex (LunarCA) <alexdefreese@gmail.com>
2023-02-26 19:02:52 -0500

    Extract button color to a function

--- src/components/util/Button.tsx
+++ src/components/util/Button.tsx
@@ -6,13 +6,26 @@ type Props = {
   color?: "primary" | "secondary" | "RED",
   flexGrow?: boolean,
 }
+
+const getColorClass = (color: Props["color"]) => {
+  switch (color) {
+    case "primary":
+      return "bg-orange-500"
+    case "secondary":
+      return "bg-cyan-600"
+    case "RED":
+      return "bg-RED"
+    default:
+      return "bg-orange-500"
+  }
+}
+
 const Button: Component<Props> = (props) => {
   const c = children(() => props.children)
-  const colorClass = props.color === "secondary" ? "bg-cyan-600" : (props.color === "primary" ? "bg-orange-500" : "bg-RED")
 
   return (
     <button 
-      class={`${colorClass} box-content m-2 p-2 ${props.flexGrow && "flex-grow"} text-center ${props.color === "RED" ? "font-extrabold text-black shadow-lg" : "text-slate-900 font-bold shadow-sm"} `}
+      class={`${getColorClass(props.color)} box-content m-2 p-2 ${props.flexGrow && "flex-grow"} text-center ${props.color === "RED" ? "font-extrabold text-black shadow-lg" : "text-slate-900 font-bold shadow-sm"} `}
       onClick={props.onClick}
     >{c()}</button>
   )
